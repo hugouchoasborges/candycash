b534c59cab15212ffb276940d7b938ab5b894519
Hugo Borges <hugouchoas@outook.com>
2020-10-31 23:48:23 -0300

    [Dev_2010292236] Added prototype base structure

---
+++ devween2/Assets/Scripts/prototype/PrototypeManager.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Create by Hugo Uchoas Borges <hugouchoas@outlook.com>
+ */
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using util;
+
+namespace prototype
+{
+    public class PrototypeManager : MonoBehaviour
+    {
+        [SerializeField] private Canvas _mainCanvas;
+
+        public void LoadPrototype(int index)
+        {
+            GameDebug.Log($"Loading Prototype {index}...");
+            _mainCanvas.enabled = false;
+            SceneManager.LoadSceneAsync($"Prototype {index}", LoadSceneMode.Additive);
+        }
+    }
+}
---
+++ devween2/Assets/Scripts/util/GameDebug.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Create by Hugo Uchoas Borges <hugouchoas@outlook.com>
+ */
+
+using UnityEngine;
+
+namespace util
+{
+    public class GameDebug : Singleton<GameDebug>
+    {
+        // ----------------------------------------------------------------------------------
+        // ========================== System Log ============================
+        // ----------------------------------------------------------------------------------
+
+        [SerializeField] private bool systemLogEnabled = true;
+
+        private void Awake()
+        {
+            updateLogConf();
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            updateLogConf();
+        }
+#endif
+
+        private void updateLogConf()
+        {
+#if UNITY_EDITOR
+            Debug.unityLogger.logEnabled = systemLogEnabled;
+#else
+            Debug.unityLogger.logEnabled = false;
+#endif
+        }
+
+
+        // ----------------------------------------------------------------------------------
+        // ========================== Logging Methods ============================
+        // ----------------------------------------------------------------------------------
+
+        [SerializeField] private bool logEnabled = true;
+
+        public static void Log(string message)
+        {
+            if (Instance.logEnabled) Debug.Log(message);
+        }
+
+    }
+}
